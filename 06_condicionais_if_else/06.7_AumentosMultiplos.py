07135e5b424df582a80f5c8aed207d625f51c0eb
whoami90 <ferreirabnaftali@gmail.com>
2022-08-02 09:52:07 -0300

    Fim do mundo 1 - Python

--- 06_condicionais_if_else/06.7_AumentosMultiplos.py
+++ 06_condicionais_if_else/06.7_AumentosMultiplos.py
@@ -3,7 +3,7 @@ Escreva um programa que pergunte o salário de um funcionário e calcule o valor
 Para salários superiores a R$1.200,00, calcule um aumento de 10%. 
 Para os salários inferiores ou iguais o aumento é de 15%. 
 '''
-salario = float(input('Qual é o valor de salário do funcionário? R$'))
+salario = float(input('Qual é o valor de salário do funcionário? R$ '))
 # Verificação se o funcionário ganha mais de R$1.200 de salário.
 if salario >= 1200:
     print('O salário é de R${:.2f}, com o aumento vai para R${:.2f}'.format(salario, ((salario * 10) / 100 + salario)))
