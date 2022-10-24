f2cd687b37f2d44fed1e210b5b6405f995cab09a
whoami90 <ferreirabnaftali@gmail.com>
2022-10-24 00:24:13 -0300

    commit de outubro

--- resume/16.loja_v2.0.py
+++ resume/16.loja_v2.0.py
@@ -6,29 +6,53 @@
 # comprar apenas galões de 3,6 litros;
 # misturar latas e galões, de forma que o desperdício de tinta seja menor. 
 # Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
+
 import math
 
-# área a ser pintada 
-area = float(input('Qual o tamanho da área a ser pintada em m²: '))
-
-# cobertura da tinta
-uso_1l = 6
-
-# latas 
+# input
+area = float(input('Digite aqui a área a ser pintada em m²: ')) # total da área em m² a ser pintada. 
+area_pin = 6  # cada litro cobre 6m² de área. 
 lata = 18
 galao = 3.6
+lata_valor = 80
+galao_valor = 25
+total_tinta_a_usar = (area / area_pin) +  (area / area_pin * (10/100)) # Foi acrescentado 10% de folga no total de tinta utilizado.
+galoes = math.ceil(total_tinta_a_usar / 3.6)
+latas_utilizadas = int(total_tinta_a_usar / 18) # para chegar ao valor de quantas latas de 18L são necessárias para pintar a área eliminando o resto.
+galoes_utilizados = ((((total_tinta_a_usar % 18) / 10) * 18) / 3.6) # para chegar ao valor de quantos galões de 3.6L são necessários para pintar a área, eliminando o resto.
 
-# valor das latas
-valor_lata = 80
-valor_galao = 25
 
-# quantidade de litros de tinta a ser utilizada
-quantidade_utilizada = (area / uso_1l)
+# condicionais e output 
+if total_tinta_a_usar < 18:
+    print('Para a área de {}m², recomendamos o galão de 3.6 litros, é a quantidade adequada para que não deixa desperdícios.'.format(area))
+    if galao == 1:
+        print('Será necessário apenas 1 galão para pintar a área desejada.')
+        print('O total da compra é de R${:.2f}'.format(galao_valor))
+    elif galao > 1:
+        print('Serão necessários {} galões para pintar a área desejada.'.format(int(galoes)))
+        print('O total da compra é de R${:.2f}'.format(galoes * 25))
 
-# condicional para definir se será utilizado latas ou galão
-if quantidade_utilizada > 0 and quantidade_utilizada <= 3.6:
-    print('A quantidade de tinta necessária para pintar a área desejada é de {:.2f}'.format(quantidade_utilizada))
-    print('Para esta quantidade indicamos o galão de 3,6 litros, ele está custando R${:.2f}'.format(valor_galao))
-elif quantidade_utilizada > 3.6 and quantidade_utilizada < 18:
-    print('A quantidade de tinta necessária para pintar a área desejada é de {:.2f}'.format(quantidade_utilizada))
-    print('Para esta quantidade será necessário {} galoes de 3.6L, o valor é R$ {:.2f}'.format((quantidade_utilizada / 3.6), 25 * (quantidade_utilizada / 3.6)))
+elif total_tinta_a_usar == 18:
+    print('Para a área de {}m², recomendamos a lata de 18 litros, é a quantidade adequada e que não deixa desperdícios')
+    print('Será necessário apenas 1 lata de 18 litros para pintar a área desejada.')
+    print('O total da compra é de R${:.2f}'.format(lata_valor))
+
+elif total_tinta_a_usar > 18:
+    if lata == 1:
+        if galao == 1:
+            print('Para a área de {}m², é necessário {} lata de 18L e {} galão de 3.6L.'.format(area, latas_utilizadas, galoes_utilizados))
+            print('O valor da compra é de {:.2f}'.format((latas_utilizadas * lata_valor) + (galoes_utilizados * galao_valor)))
+        elif galao > 1:
+            print('Para a área de {}m², será necessário {} lata de 18L e {} galões de 3.6L.'.format(area, latas_utilizadas, galoes_utilizados))
+            print('O valor da compra é de {:.2f}'.format((latas_utilizadas * lata_valor) + (galoes_utilizados * galao_valor)))
+    elif lata > 1:
+        if galao == 1:
+            print('Para a área de {}m², será necessário {} latas de 18L e {} galão de 3.6L. '.format(area, latas_utilizadas, galoes_utilizados))
+            print('O valor da compra é de {:.2f}'.format((latas_utilizadas * lata_valor) + (galoes_utilizados * galao_valor)))
+        elif galao > 1:
+            print('Para a área de {}m², será necessário {} latas de 18L e {} galões de 3.6L.'.format(area, latas_utilizadas, galoes_utilizados))
+            print('O valor da compra é de {:.2f}'.format((latas_utilizadas * lata_valor) + (galoes_utilizados * galao_valor)))
+   
+else:
+    print('Error!')
+
